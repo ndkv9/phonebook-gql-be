706001c5d4dd8fdebb92bbd92bccb3cde831ee5f
ndkv9 <vu.nguyen101@gmail.com>
2021-03-11 13:29:04 +0200

    modify editPerson

--- index.js
+++ index.js
@@ -99,9 +99,7 @@ const resolvers = {
 			}
 
 			const updatedPerson = { ...person, phone: args.phone }
-			persons = persons.map(p =>
-				p.name === args.name ? updatedPerson : person
-			)
+			persons = persons.map(p => (p.name === args.name ? updatedPerson : p))
 			return updatedPerson
 		},
 	},
