d8910c98b629fa0099713ef779495d7bb8c0991e
ndkv9 <vu.nguyen101@gmail.com>
2021-03-13 14:45:17 +0200

    add addAsFriend mutation

--- index.js
+++ index.js
@@ -1,4 +1,9 @@
-const { ApolloServer, UserInputError, gql } = require('apollo-server')
+const {
+	ApolloServer,
+	UserInputError,
+	gql,
+	AuthenticationError,
+} = require('apollo-server')
 const mongoose = require('mongoose')
 const Person = require('./models/person')
 const jwt = require('jsonwebtoken')
@@ -68,6 +73,7 @@ const typeDefs = gql`
 		editNumber(name: String!, phone: String!): Person
 		createUser(username: String!): User
 		login(username: String!, password: String!): Token
+		addAsFriend(name: String!): User
 	}
 `
 const resolvers = {
@@ -94,11 +100,18 @@ const resolvers = {
 		},
 	},
 	Mutation: {
-		addPerson: async (root, args) => {
+		addPerson: async (root, args, context) => {
 			const person = new Person({ ...args })
+			const currentUser = context.currentUser
+
+			if (!currentUser) {
+				throw new AuthenticationError('not authenticated')
+			}
 
 			try {
 				await person.save()
+				currentUser.friends = currentUser.friends.concat(person)
+				await currentUser.save()
 			} catch (error) {
 				throw new UserInputError(error.message, { invalidArgs: args })
 			}
@@ -141,6 +154,23 @@ const resolvers = {
 			}
 
 			return { value: jwt.sign(userForToken, JWT_SECRET) }
+		},
+		addAsFriend: async (root, args, { currentUser }) => {
+			const nonFriendAlready = person =>
+				!currentUser.friends.map(f => f._id).includes(person._id)
+
+			if (!currentUser) {
+				throw new AuthenticationError('not authenticated!')
+			}
+
+			const person = await Person.findOne({ name: args.name })
+			if (nonFriendAlready(person)) {
+				currentUser.friends = currentUser.friends.concat(person)
+			}
+
+			await currentUser.save()
+
+			return currentUser
 		},
 	},
 }
